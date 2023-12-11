e82786a0e247954c76ab71b25462fbac2b758b40
WTR-GITHUB <123814736+WTR-GITHUB@users.noreply.github.com>
2023-12-11 14:51:04 +0200

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,26 +1,35 @@
 import os
 
-meniu_text = f"""Welcome to our restaurant:
+def print_from_file(file_name: str, start_sign: str, end_sign: str) -> None:
+    with open(file_name, "r", encoding="utf-8") as file:
+        is_between_signs = False
+    
+        for line in file:
+            if start_sign in line:
+                is_between_signs = True
+                continue
+            elif end_sign in line:
+                is_between_signs = False
+                break
+            
+            if is_between_signs:
+                print(line.strip())
 
-Enter '1' to get info about us.
-Enter '2' to see menu and order.
-Enter '3' for table reservations and administration.
-Enter '4' pay and make it happen.
-Enter 'q' to exit program.
-"""
 wrong_key = ""
 
+os.system("cls||clear")
+print_from_file(file_name="test.txt", start_sign="<<1>>", end_sign="<<2>>")
 
 while True:
-    print(meniu_text)
-    user_key = input()
+
+    user_key = input().lower()
 
     if user_key == "q":
         os.system("cls||clear")
         exit()
     elif user_key == "1":
         os.system("cls||clear")
-        print("This will be information about us")
+        print_from_file("test.txt", "<<2>>", "<<3>>")
     elif user_key == "2":
         os.system("cls||clear")
         print("This will be food menu and order information")
@@ -30,6 +39,11 @@ while True:
     elif user_key == "4":
         os.system("cls||clear")
         print("Pay and go")
+    elif user_key == "b":
+        os.system("cls||clear")
+        print_from_file(file_name="test.txt", start_sign="<<1>>", end_sign="<<2>>")
     else:
         wrong_key = f"You have entered '{user_key}' a bad key, please do better."
         print(wrong_key)
+
+
