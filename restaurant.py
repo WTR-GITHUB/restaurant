04f223680bc18bdea228ea640aaa80a8c1dc5377
Saulius Anusas <saulius.anusas@gmail.com>
2023-12-11 17:56:35 +0200

    Padariau

--- restaurant.py
+++ restaurant.py
@@ -1,18 +1,29 @@
 import os
 
-meniu_text = f"""Welcome to our restaurant:
 
-Enter '1' to get info about us.
-Enter '2' to see menu and order.
-Enter '3' for table reservations and administration.
-Enter '4' pay and make it happen.
-Enter 'q' to exit program.
-"""
+class Menu:
+    MAIN_MENU_TEXT = f"""Welcome to our restaurant:
+
+    Enter '1' to get info about us.
+    Enter '2' to see menu and order.
+    Enter '3' for table reservations and administration.
+    Enter '4' pay and make it happen.
+    Enter 'q' to exit program.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def main_menu(self):
+        return self.MAIN_MENU_TEXT
+
+
+menu = Menu()
+
 wrong_key = ""
-
+print(menu.main_menu())
 
 while True:
-    print(meniu_text)
     user_key = input()
 
     if user_key == "q":
@@ -30,6 +41,11 @@ while True:
     elif user_key == "4":
         os.system("cls||clear")
         print("Pay and go")
+    elif user_key == "b":
+        os.system("cls||clear")
+        print(menu.main_menu())
     else:
+        os.system("cls||clear")
+        print(menu.main_menu())
         wrong_key = f"You have entered '{user_key}' a bad key, please do better."
         print(wrong_key)
