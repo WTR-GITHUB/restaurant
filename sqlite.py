7ff3772314ce30d6d836453de623c7d921e6ac6d
Saulius Anusas <saulius.anusas@gmail.com>
2024-01-03 20:22:10 +0200

    Cleaned some trash

--- sqlite.py
+++ sqlite.py
@@ -4,13 +4,13 @@ conn = sqlite3.connect("restaurant.db")
 c = conn.cursor()
 
 
-def fetch_categories() -> list:
+def fetch_categories() -> dict:
     c.execute("SELECT Category FROM menu")
     my_dict = {i + 1: item[0] for i, item in enumerate(c)}
     return my_dict
 
 
-def fetch_food_by_category(mark: int) -> list:
+def fetch_food_by_category(mark: int) -> dict:
     c.execute("SELECT food_name, price FROM food WHERE id_Category = ?", (mark,))
     my_dict = {i + 1: item for i, item in enumerate(c)}
     return my_dict
